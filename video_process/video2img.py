76c427d930306733abb1cff0d0092ccfb062144e
aurora <auroua@yeah.net>
2016-02-15 16:19:09 +0800

    viedo2img  v0.1

--- video_process/video2img.py
+++ video_process/video2img.py
@@ -10,28 +10,28 @@ def create_floders(urls):
 
 if __name__=='__main__':
     url = '/home/aurora/hdd/video/'
+    # filenames = '/home/aurora/hdd/video/20150916/ch01_00000000066000000.mp4'
     urls = [os.path.join(url, dir) for dir in os.listdir(url) if not 'npy' in dir]
     for video_url in urls:
         save_urls = video_url+'_npy/'
         print save_urls
         videos = [os.path.join(video_url, video) for video in os.listdir(video_url)]
+        i = 0
         for video in videos:
-            filename = video.split('/')[-1][0:-4]
-            save_dir = save_urls + filename
-            print save_dir,
+            # filename = video.split('/')[-1][0:-4]
+            # save_dir = save_urls + filename
+            print video
             cap = cv2.VideoCapture(video)
-            frames = []
             while True:
+                # frame = cv.QueryFrame(cap)
                 ret, im = cap.read()
                 # blur = cv2.GaussianBlur(im, (0, 0), 5)
-                cv2.imshow('video test', im)
-                frames.append(im)
-                key = cv2.waitKey(10)
-                if key == 1048603:
+                # cv2.imshow('video test', im)
+                # frames.append(im)
+                if not ret:
                     break
-            frames = np.array(frames)
-            np.save(save_dir, frames)
-            # print im.shape
-            print 'file shape is ', frames.shape
+                cv2.imwrite(save_urls+'pic{:>05}.jpg'.format(i), im)
+                key = cv2.waitKey(10)
+                i += 1
             cap.release()
         cv2.destroyAllWindows()
